2b0d22680cbb23206a51b6e91936390fc93c2f7e
Muhammad <shalauddinahmedshipon@gmail.com>
2024-02-19 14:09:17 -0800

    script file commit

---
+++ js/script.js
@@ -0,0 +1,107 @@
+const allSeats = document.getElementsByClassName("seat-btn");
+let selectedSeats = 0;
+let leftSeats = 40;
+const selectArr =[];
+let total = 0;
+let count = 0;
+document.getElementById('coupon-button').setAttribute('disabled',true)
+for(const seat of allSeats){
+  const Element = seat.innerText ;
+  
+  seat.addEventListener('click',function(event){
+   
+      count = count + 1 ;
+     if(count <= 4){
+        event.target.classList.add('bg-green-300');
+        selectedSeats = selectedSeats + 1 ;
+       setInnerText('selected-seats',selectedSeats);
+    
+       leftSeats = leftSeats - 1 ;
+    setInnerText('seats-left',leftSeats);
+
+    const tBody = document.getElementById('t-body');
+const tr = document.createElement('tr')
+const td1 = document.createElement('td');
+const td2 = document.createElement('td');
+const td3 = document.createElement('td');
+td1.innerText = Element;
+td2.innerText = "Economics";
+td3.innerText = 550;
+
+tr.appendChild(td1);
+tr.appendChild(td2);
+tr.appendChild(td3);
+
+tBody.appendChild(tr)
+
+total = total + 550 ;
+const totalPrice = document.getElementById('total-price');
+totalPrice.innerText = total; 
+const grandTotal = document.getElementById('grand-total-price');
+grandTotal.innerText = total;
+
+
+document.getElementById('coupon-field').addEventListener('keyup',function(){
+  const field = document.getElementById('coupon-field');
+    const text = field.value ;
+    console.log(text);
+    if(text === 'NEW15'){
+      document.getElementById('coupon-button').removeAttribute('disabled').addEventListener('click',
+      function(){
+        const discount = total * .15 ;
+        const grandTotal = total - discount ;
+      
+      }
+    
+      
+      );
+      
+    }
+
+    else if( text === 'Couple 20'){document.getElementById('coupon-button').removeAttribute('disabled').addEventListener('click',
+    function(){
+      const discount = total * .20 ;
+      const grandTotal = total - discount ;
+    })
+    }
+    else{document.getElementById('coupon-button').setAttribute('disabled',true)}
+ })
+  
+
+
+      }
+
+
+
+
+
+const couponButton = document.getElementById("coupon-button");
+console.log(count);
+if(count < 4){
+  couponButton.setAttribute('disabled',true);
+
+  }
+  
+  else{
+    couponButton.removeAttribute('disabled')
+  }
+
+      }
+
+    
+      
+    
+
+     
+    
+
+
+  )
+}
+
+function setInnerText(id,value){
+  document.getElementById(id).innerText = value;
+}
+ 
+ 
+
